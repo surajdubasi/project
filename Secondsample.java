12bd1ea72f2f034cd7b0084aed2e58dd28ec5a66
Sunny <sunnysuraj2001@gmail.com>
2025-02-22 16:03:52 -0700

    added one file and commiting

---
+++ Secondsample.java
@@ -0,0 +1,24 @@
+class  
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		//this is a sample code which we are trying to modify and practice with git commands 
+		//I am learning git commands 
+		// I learnt how to clone the repository 
+		/* I learn how to check status of git 
+		by calling git status 
+		i learned how to add and commit 
+		git add #file path or git add * 
+		then to commit 
+		git commit -m "This is a commit "
+		git push then it will go into remote repository that is github 
+		git pull to call the files from remote repository to local repository
+		git diff #filepath 
+		it is showing the lines of code whstever we have changed from the begining 
+		git status : whilll show the status of the file 
+		git log : shows what we have committed prebviously 
+		git branch : shows main/master or anyother branch 
+		
+	}
+}
